79482f1258d54d3476453ac1777877fd218aee47
IvanKhartanovich <ivan_khart@mail.ru>
2024-10-21 21:55:25 +0300

    1. Binding in Worklist.controller.js

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -3,8 +3,16 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"zjblessons/Worklist/model/formatter",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+	"sap/ui/model/Sorter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/Text"
+], function (BaseController,
+	JSONModel,
+	formatter,
+	Filter,
+	Sorter,
+	FilterOperator,
+	Text) {
 	"use strict";
 
 	return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
@@ -16,10 +24,54 @@ sap.ui.define([
 			this.setModel(oViewModel, "worklistView");
 		},
 
+
+		onBeforeRendering: function () {
+			this._bindTable();
+		},
+
 		onLiveChangeDNumberSField: function (oEvent) {
 			const sValue = oEvent.getParameter('newValue');
 
 			this._searchHandler('DocumentNumber', FilterOperator.Contains, sValue);
+		},
+
+		_bindTable: function () {
+			const oTable = this.getView().byId('table');
+
+			oTable.bindItems({
+				path: '/zjblessons_base_Headers',
+				sorter: [new Sorter('DocumentDate', true)],
+				template: this._getTableTemplate(),
+				templateSharable: true,
+				urlParameters: {
+					$select: 'HeaderID,DocumentNumber,DocumentDate,PlantText,Description,Created'
+				},
+				events: {
+					dataReceived: (oData) => { debugger },
+					dataRequested: (oData) => { debugger },
+				}
+			});
+		},
+
+		_getTableTemplate: function () {
+			const oTemplate = new sap.m.ColumnListItem({
+				type: 'Navigation',
+				navigated: true,
+				cells: [
+					new sap.m.Text({
+						text: '{DocumentNumber}'
+					}), new sap.m.Text({
+						text: '{DocumentDate}'
+					}), new sap.m.Text({
+						text: '{PlantText}'
+					}), new sap.m.Text({
+						text: '{Description}'
+					}), new sap.m.Text({
+						text: '{Created}'
+					}),
+				]
+			});
+			return oTemplate;
 		},
 
 		searchPTextSField: function (oEvent) {
